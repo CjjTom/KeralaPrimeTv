85840366f1dab75bf87b7530629ac8a7b290b029
CjjTom <keralaprime1@gmail.com>
2025-07-09 16:16:30 +0530

    Update render_template.py

--- TechVJ/util/render_template.py
+++ TechVJ/util/render_template.py
@@ -11,66 +11,65 @@ from pyrogram.errors import FloodWait
 from TechVJ.util.file_properties import get_name, get_hash, get_media_file_size, get_file_ids
 from plugins.database import db
 
-async def render_page(id, user, secid, thid, src=None):
-    file_data_one = None
-    file_data_two = None
-    file_data_three = None
-    if id != 0:
-        try:
-            fileone = await TechVJBot.get_messages(int(LOG_CHANNEL), int(id))
-        except:
-            fileone = await TechVJBackUpBot.get_messages(int(LOG_CHANNEL), int(id))
-        file_data_one = await get_file_ids(fileone)
+async def render_page(id, user, secid=None, thid=None, fourid=None, fiveid=None, src=None):
+    file_data = None
+    file_urls = {}
+    quality_map = {
+        '360': None,
+        '420': None,
+        '480': None,
+        '540': None,
+        '720': None,
+        '1080': None
+    }
     
-        src = urllib.parse.urljoin(
-            STREAM_URL + "dl/",
-            f"{id}/{urllib.parse.quote_plus(file_data_one.file_name)}?hash={file_data_one.unique_id[:6]}",
-        )
-        quality = "480"
-    else:
-        src = None
-        quality = None
-
-    if secid != 0:
-        try:
-            filetwo = await TechVJBot.get_messages(int(LOG_CHANNEL), int(secid))
-        except:
-            filetwo = await TechVJBackUpBot.get_messages(int(LOG_CHANNEL), int(secid))
-        file_data_two = await get_file_ids(filetwo)
-        file_url_two = urllib.parse.urljoin(
-            STREAM_URL + "dl/",
-            f"{secid}/{urllib.parse.quote_plus(file_data_two.file_name)}?hash={file_data_two.unique_id[:6]}",
-        )
-        quality_two = "720"
-    else:
-        file_url_two = None
-        quality_two = None
-
-    if thid != 0:
-        try:
-            filethree = await TechVJBot.get_messages(int(LOG_CHANNEL), int(thid))
-        except:
-            filethree = await TechVJBackUpBot.get_messages(int(LOG_CHANNEL), int(thid))
-        file_data_three = await get_file_ids(filethree)
-        file_url_three = urllib.parse.urljoin(
-            STREAM_URL + "dl/",
-            f"{thid}/{urllib.parse.quote_plus(file_data_three.file_name)}?hash={file_data_three.unique_id[:6]}",
-        )
-        quality_three = "1080"
-    else:
-        file_url_three = None
-        quality_three = None
-        
-    if file_data_one == None:
-        if file_data_two == None:
-            file_data = file_data_three
+    # Map input IDs to their respective qualities
+    id_mapping = {
+        '360': id,
+        '420': secid,
+        '480': thid,
+        '540': fourid,
+        '720': fiveid,
+        '1080': src  # Note: src is being repurposed here for 1080p
+    }
+    
+    # Process each quality
+    for quality, msg_id in id_mapping.items():
+        if msg_id and msg_id != '0':
+            try:
+                msg = await TechVJBot.get_messages(int(LOG_CHANNEL), int(msg_id))
+            except:
+                msg = await TechVJBackUpBot.get_messages(int(LOG_CHANNEL), int(msg_id))
+                
+            file_data = await get_file_ids(msg)
+            
+            file_urls[f'file_url_{quality}'] = urllib.parse.urljoin(
+                STREAM_URL + "dl/",
+                f"{msg_id}/{urllib.parse.quote_plus(file_data.file_name)}?hash={file_data.unique_id[:6]}",
+            )
+            quality_map[quality] = quality
         else:
-            file_data = file_data_two
-    else:
-        file_data = file_data_one
-        
+            file_urls[f'file_url_{quality}'] = None
+    
+    # Determine the main file data to use for metadata
+    if not file_data:
+        # Try to get any available file data
+        for msg_id in [id, secid, thid, fourid, fiveid, src]:
+            if msg_id and msg_id != '0':
+                try:
+                    msg = await TechVJBot.get_messages(int(LOG_CHANNEL), int(msg_id)
+                except:
+                    msg = await TechVJBackUpBot.get_messages(int(LOG_CHANNEL), int(msg_id)
+                file_data = await get_file_ids(msg)
+                if file_data:
+                    break
+    
+    if not file_data:
+        raise InvalidHash("No valid file data found")
+    
     tag = file_data.mime_type.split("/")[0].strip()
     file_size = humanbytes(file_data.file_size)
+    
     if tag in ["document", "video", "audio"]:
         template_file = "TechVJ/template/req.html"
     else:
@@ -87,16 +86,21 @@ async def render_page(id, user, secid, thid, src=None):
     file_name = remove_after_year(file_name_clean)
     link = await db.get_link(int(user))
     name = await db.get_name(int(user))
-    return template.render(
-        file_name=file_name,
-        file_url=src,
-        file_url_two=file_url_two,
-        file_url_three=file_url_three,
-        file_size=file_size,
-        user_id=user,
-        link=link,
-        name=name
-    )
+    
+    # Prepare template context
+    context = {
+        "file_name": file_name,
+        "file_size": file_size,
+        "user_id": user,
+        "link": link,
+        "name": name,
+        "thumbnail_url": THUMBNAIL_URL  # Make sure THUMBNAIL_URL is defined in your info.py
+    }
+    
+    # Add all quality URLs to context
+    context.update(file_urls)
+    
+    return template.render(**context)
 
 
 def clean_file_name(file_name):
@@ -115,4 +119,5 @@ def remove_after_year(filename):
         year_index = match.start()
         year_end_index = match.end()
         new_filename = filename[:year_end_index].strip()
-        return new_filename 
+        return new_filename
+    return filename
